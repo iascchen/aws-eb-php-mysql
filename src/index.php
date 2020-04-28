8b2c2cee679750bc400b8ea0cb4b6bdc6eeb148b
Hao Chen <chenhao@microduino.cc>
2020-04-28 22:55:52 +0800

    add db info

--- src/index.php
+++ src/index.php
@@ -7,5 +7,17 @@
  <?php echo '<p>Run PHP in Docker of Elastic Beanstalk</p>'; ?>
  <?php echo '<p>CHEN Hao for AWS Assignment</p>'; ?>
  <?php phpinfo(); ?>
+ <?php
+   define('DB_HOST', getenv('RDS_HOSTNAME'));
+   define('DB_PORT', getenv('RDS_PORT'));
+   define('DB_USER', getenv('RDS_USERNAME'));
+   define('DB_NAME', getenv('RDS_DB_NAME'));
+
+   echo $DB_HOST;
+   echo $DB_PORT;
+   echo $DB_USER;
+   echo $DB_NAME;
+ ?>
+ <?php echo DB_NAME; ?>
  </body>
 </html>
