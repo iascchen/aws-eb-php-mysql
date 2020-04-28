56b696ff46ad354d28ae37ce32ea26ac5b67eb93
Hao Chen <chenhao@microduino.cc>
2020-04-28 23:11:08 +0800

    add db info

--- src/index.php
+++ src/index.php
@@ -11,19 +11,19 @@
      define("CONSTANT", "<h3>DB Info</h3>");
      echo CONSTANT;
     ?>
-  <p>
   <?php
    define('DB_HOST', getenv('RDS_HOSTNAME'));
    define('DB_PORT', getenv('RDS_PORT'));
    define('DB_USER', getenv('RDS_USERNAME'));
    define('DB_NAME', getenv('RDS_DB_NAME'));
 
-   echo DB_HOST;
-   echo DB_PORT;
-   echo DB_USER;
+   echo '<p>';
+   echo DB_HOST; echo ' , ';
+   echo DB_PORT; echo ' , ';
+   echo DB_USER; echo ' , ';
    echo DB_NAME;
+   echo '</p>';
   ?>
- </P>
  <hr/>
   <?php phpinfo(); ?>
  </body>
