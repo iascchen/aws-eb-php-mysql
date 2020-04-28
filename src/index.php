d3a865039d5e2d8456d75e5cade7424c24ed21b1
Hao Chen <chenhao@microduino.cc>
2020-04-28 23:18:05 +0800

    add db info

--- src/index.php
+++ src/index.php
@@ -15,6 +15,7 @@
    define('DB_HOST', getenv('RDS_HOSTNAME'));
    define('DB_PORT', getenv('RDS_PORT'));
    define('DB_USER', getenv('RDS_USERNAME'));
+   define('RDS_PASSWORD', getenv('RDS_PASSWORD'));
    define('DB_NAME', getenv('RDS_DB_NAME'));
 
    echo '<p>';
@@ -23,6 +24,16 @@
    echo DB_USER; echo ' , ';
    echo DB_NAME;
    echo '</p>';
+
+   // Create connection
+   $conn = new mysqli($DB_HOST, $DB_USER, $RDS_PASSWORD);
+
+   // Check connection
+   if ($conn->connect_error) {
+       die("Connection failed: " . $conn->connect_error);
+   }
+   echo "Connected successfully";
+   ?>
   ?>
  <hr/>
   <?php phpinfo(); ?>
